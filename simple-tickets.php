988c77cc3ae85a5a2aa6bd49644d9faa3f7d1f2f
Serafim Bogomolov <sbog787@gmail.com>
2019-05-19 18:40:28 +0700

    Init the project

---
+++ simple-tickets.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ Plugin Name: Simple tickets
+ Plugin URI: https://github.com/sBog787/simple-tickets
+ Description: Plugin provides feedback functionality for visitors of your site.
+ Version: 0.0.1
+ Author: sBog787
+ */
